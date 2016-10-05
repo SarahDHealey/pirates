2df512850fee5a5eb36a8dc77cd7547fd8f558ff
Sarah Healey <s.deane.healey@gmail.com>
2016-10-05 00:24:58 -0600

    both files for the decoupled pirate app

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+const port = 3000
+
 var api = require('./routes/api');
 var users = require('./routes/users');
 
@@ -22,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api/pirates', api);
+app.use('/', api)
 app.use('/users', users);
 
 // catch 404 and forward to error handler
@@ -39,10 +41,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
+    res.send('shit!!!')
   });
 }
 
@@ -50,10 +49,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+  res.send('shit!!!')
 });
 
 
--- public/javascripts/controllers/pirates_controller.js
+++
@@ -1,6 +0,0 @@
-app.controller('PiratesController', function($scope, PiratesService) {
-  console.log('hits controller');
-  $scope.view = {};
-  $scope.view.pirates = PiratesService;
-  $scope.view.all = PiratesService.all;
-  });
